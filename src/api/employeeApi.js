1a205e45667d1f0a77a4b5a3541a418458e44381
= <=>
2024-05-18 17:34:34 +0530

    post request using axios and bootstrap forms

--- src/api/employeeApi.js
+++ src/api/employeeApi.js
@@ -9,6 +9,15 @@ const getEmployee = async() => {
     console.log("get Employee -", err);
   }
 }
+const createEmployee = async (data) => {
+  try {
+    const result = await axiosInstance.post('create', data);
+    return result.data.data;
+  } catch (err) {
+    console.log("create employee error -", err);
+  }
+}
 export {
   getEmployee,
+  createEmployee,
 };
--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import CustomTable from "../../component/CustomTable"
-import { getEmployee } from "../../api/employeeApi";
+import { createEmployee, getEmployee } from "../../api/employeeApi";
+import { Button, Form } from "react-bootstrap";
 
 const Employee = () => {
   const [employeeData, setEmployeeData] = useState([]);
-  const headers = ["Name", "Salary", "Age", "Profile Image"];
+  const headers = ["Emp Id", "Name", "Salary", "Age", "Profile Image"];
   useEffect(() => {
     fetchEmployeeData();
   }, []);// If Empty array, then useEffect will run only once after the page load
@@ -15,8 +16,47 @@ const Employee = () => {
       setEmployeeData(response);
     }
   }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const salary = e.target[1].value;
+    const age = Number(e.target[2].value);
+    console.log(name, salary, age);
+    if(name.length > 0 && salary.length > 0 && age > 0) {
+      const result = await createEmployee({
+        name: name,
+        salary: salary,
+        age: `${age}`
+      });
+      console.log('result -', result);
+      if(result && result.id) {
+        // setEmployeeData([...employeeData, result]);
+        const data = employeeData.push(result);
+        setEmployeeData(data);
+      }
+    }
+  }
   return (
-    <CustomTable headers={headers} data={employeeData} />
+    <>
+      <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3" controlId="emp_name">
+        <Form.Label>Employee Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="emp_salary">
+        <Form.Label>Salary</Form.Label>
+        <Form.Control type="text" placeholder="Enter Salary" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="emp_age">
+        <Form.Label>Age</Form.Label>
+        <Form.Control type="number" placeholder="Enter Age" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      <CustomTable headers={headers} data={employeeData} />
+    </>
   )
 };
 export default Employee;
